f5f25623c3bd7eb4a5402c69f86e9ea7e69e3ce2
Mazzi <lor.bellino@gmail.com>
2020-09-17 23:47:18 +0200

    created snake class added movement

--- sketch.js
+++ sketch.js
@@ -1,13 +1,33 @@
 
 let snake;
+let resolution = 10;
 
 function setup() {
-  createCanvas(600,000);
+  createCanvas(600,600);
+  //frameRate(15);
   snake = new Snake();
 }
 
 function draw() {
-  background(255,20,200);
+  background(220);
+  scale(resolution);
   snake.update();
   snake.show();
+}
+
+function keyPressed(){
+  switch(keyCode){
+    case LEFT_ARROW:
+      snake.setDir(-1,0);
+      break;
+    case RIGHT_ARROW:
+      snake.setDir(1,0);
+      break;
+    case UP_ARROW:
+      snake.setDir(0,-1);
+      break;
+    case DOWN_ARROW:
+      snake.setDir(0,1);
+      break;
+  }
 }
--- snake.js
+++ snake.js
@@ -1,16 +1,25 @@
 class Snake {
 
   constructor(){
-
+    this.xdir=0;
+    this.ydir=0;
+    this.body = [];
+    this.body[0] = createVector(0,0);
   }
 
   update (){
-
+    this.body[0].x += this.xdir;
+    this.body[0].y += this.ydir;
 
   }
 
   show() {
-
+    rect(this.body[0].x,this.body[0].y,1,1)
     
+  }
+
+  setDir(x,y){
+    this.xdir=x/5;
+    this.ydir=y/5;
   }
 }
